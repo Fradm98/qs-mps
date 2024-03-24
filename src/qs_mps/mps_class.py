df84f5796ef38c985026a272c6e8aab9819bb292
Fradm98 <fradm@kth.se>
2024-03-24 20:45:05 +0100

    pc

--- src/qs_mps/mps_class.py
+++ src/qs_mps/mps_class.py
@@ -527,7 +527,7 @@ class MPS:
             label_env = up + down + mid_up + mid_down
             idx += 1
 
-        mps_dm = ncon([env_l, env_r], [[label_env], [1, 2]])
+        mps_dm = ncon([env_l, env_r], [label_env, [1, 2]])
 
         return mps_dm
 
@@ -548,6 +548,9 @@ class MPS:
 
         elif self.model == "ANNNI":
             self.mpo_ANNNI()
+        
+        elif self.model == "Cluster":
+            self.mpo_Cluster()
 
         elif self.model == "Z2_dual":
             self.Z2.mpo_Z2_ladder_generalized()
@@ -757,6 +760,9 @@ class MPS:
         
         elif self.model == "ANNNI":
             self.order_param_Ising(op=op)
+        
+        elif self.model == "Cluster":
+            self.order_param_Ising(op=op)
 
         elif self.model == "Z2_two_ladder":
             self.order_param_Z2()
@@ -779,7 +785,10 @@ class MPS:
         """
         I = np.eye(2)
         O = np.zeros((2, 2))
-        op = sparse_pauli_z(n=0, L=1).toarray()
+        if op == "Z":
+            op = sparse_pauli_z(n=0, L=1).toarray()
+        elif op == "X":
+            op = sparse_pauli_x(n=0, L=1).toarray()
         w_tot = []
         for _ in range(self.L):
             w_mag = np.array([[I, op], [O, I]])
@@ -2528,6 +2537,8 @@ class MPS:
         """
         if "Ising" in self.model:
             self.save_sites_Ising(path=path, precision=precision)
+        elif "Cluster" in self.model:
+            self.save_sites_Ising(path=path, precision=precision)
         elif "ANNNI" in self.model:
             self.save_sites_ANNNI(path=path, precision=precision)
         elif "Z2" in self.model:
@@ -2548,6 +2559,8 @@ class MPS:
 
         """
         if "Ising" in self.model:
+            self.load_sites_Ising(path=path, precision=precision)
+        elif "Cluster" in self.model:
             self.load_sites_Ising(path=path, precision=precision)
         elif "ANNNI" in self.model:
             self.load_sites_ANNNI(path=path, precision=precision)
