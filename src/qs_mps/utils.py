9b5decf01e6092b6d884dd29793190e4bc8f884a
Fradm98 <fra.di.marcationio@gmail.com>
2025-02-24 12:41:13 +0100

    include off axis calculations, get_cy and dmrg

--- src/qs_mps/utils.py
+++ src/qs_mps/utils.py
@@ -1223,8 +1223,11 @@ def get_cx(L, R):
     return [int(L / 2 - R / 2), int(L / 2 + R / 2)]
 
 
-def get_cy(l, bc):
-    if bc == "pbc":
-        return [0,0]
-    elif bc == "obc":
-        return [l // 2, l // 2]
+def get_cy(l, bc, cy: list=None):
+    if cy == None:
+        if bc == "pbc":
+            return [0,0]
+        elif bc == "obc":
+            return [l // 2, l // 2]
+    else:
+        return cy
