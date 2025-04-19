5c2a97e113a9ace26bf8eb2975da3488cfc202f3
Fradm98 <fradm@kth.se>
2025-04-19 23:22:07 +0200

    vacuum 19 and 21 have precision 3

--- src/qs_mps/applications/Z2/fidelity_susceptibility.py
+++ src/qs_mps/applications/Z2/fidelity_susceptibility.py
@@ -192,7 +192,7 @@ l = 6
 h_i, h_f, npoints = 0.6, 0.95, 15
 Rs = [10,11,12,13,14,15,16,17,18,19,20,21]
 chis = [256]
-precision = 2
+# precision = 2 not for vacuum, 19, 21
 
 # l = 6
 # h_i, h_f, npoints = 0.4, 0.6, 21
