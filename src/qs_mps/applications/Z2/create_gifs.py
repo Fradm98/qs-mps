9377dae70a3009bf63135d56eeefcc5f1c125498
Francesco Di Marcantonio <fra.di.marcantonio@gmail.com>
2026-01-26 12:38:50 +0100

    gif of the occupation as well, playing with fid susc in the roughening area

--- src/qs_mps/applications/Z2/create_gifs.py
+++ src/qs_mps/applications/Z2/create_gifs.py
@@ -31,11 +31,37 @@ target = {
     'cx': np.array([10, 30]), 'cy': np.array([0, 0]), 
     'delta': 0.05, 'h_ev': 0.8, 'h_i': 0.8, 'of': 0.3,
 }
+
+target = {
+    'L': 40, 'N': 7, 'R': 20, 'T': 200, 
+    'bc': 'pbc', 'chis': [128, 192, 256, 384, 512], 
+    'cx': [10, 30], 'cy': [0, 0], 
+    'delta': 0.05, 'h_ev': 0.8, 'h_i': 0.8, 'of': 0.3,
+}
+
 chi_t = 128
 
-efields = np.asarray(get_el_field_in_time(f"{parent_path}/results/time_data/results_time.hdf5", target, bond_dim=chi_t))
+lattice = False
+
+efields = np.asarray(get_el_field_in_time(f"{parent_path}/results/time_data/results_time_cluster.hdf5", target, bond_dim=chi_t))
 
 steps = len(efields)
+
+if lattice:
+    if save_gif:
+        movie = anim(frames=steps, interval=200, data=efields, params=np.linspace(0,steps/target['of']*target['delta'],steps+1), show=False, charges_x=target['cx'], charges_y=target['cy'], precision=2, time=True)
+        movie.save(filename=f"{path_figures}/figures/animations/quench_Z2_{target['L']}x{target['N']}_g_i_{target['h_i']}_g_ev_{target['h_ev']}_trott_steps_{steps}_delta_{target['delta']}_chi_{chi_t}.gif")
+
+else:
+    data = []
+    for field in efields:
+        hor_field = np.roll(field, shift=target['N']-1, axis=0)
+        hor_field = hor_field[::2,1::2]
+        hor_occup = (1 - hor_field) / 2
+        data.append(hor_occup)
+    data = np.array(data)
+    cy = [target['N']//2, target['N']//2]
+
 if save_gif:
-    movie = anim(frames=steps, interval=200, data=efields, params=np.linspace(0,steps/target['of']*target['delta'],steps+1), show=False, charges_x=target['cx'], charges_y=target['cy'], precision=2, time=True)
-    movie.save(filename=f"{path_figures}/figures/animations/quench_Z2_{target['L']}x{target['N']}_g_i_{target['h_i']}_g_ev_{target['h_ev']}_trott_steps_{steps}_delta_{target['delta']}_chi_{chi_t}.gif")
+    movie = anim_no_lattice(frames=steps, interval=200, data=data, params=np.linspace(0,steps/target['of']*target['delta'],steps+1), show=False, charges_x=target['cx'], charges_y=cy, precision=2, time=True)
+    movie.save(filename=f"{path_figures}/figures/animations/quench_occupation_{target['L']}x{target['N']}_g_start_{target['h_i']}_g_ev_{target['h_ev']}_trott_steps_{target['T']}_delta_{target['delta']}_chi_{chi_t}.gif")
